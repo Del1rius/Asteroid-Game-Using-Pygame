847f87f75ba6f0ff85066d0489ee8b6608468d1f
Del1rius <pleb.gaming.yt@gmail.com>
2025-12-03 13:19:51 +0200

    added asteroid and asteroidfield with random asteroids spawning

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -18,13 +20,17 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while True:
+    field = AsteroidField()
 
+    while True:
         log_state()
 
         for event in pygame.event.get():
@@ -35,8 +41,8 @@ def main():
 
         screen.fill("black")
 
-        for thing in drawable:
-            thing.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
